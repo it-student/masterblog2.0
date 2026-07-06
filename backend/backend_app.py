5b72b6a0064f05cc9fc445e726a4a88f74bbfdf0
Yasin Akkus <akkusoft@gmail.com>
2026-07-06 22:21:50 +0200

    'Add' functionality created. Saving the next available unique id within the data schema itself and updating it only with every post creation.

--- backend/backend_app.py
+++ backend/backend_app.py
@@ -1,19 +1,66 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from flask_cors import CORS
 
 app = Flask(__name__)
 CORS(app)  # This will enable CORS for all routes
 
-POSTS = [
-    {"id": 1, "title": "First post", "content": "This is the first post."},
-    {"id": 2, "title": "Second post", "content": "This is the second post."},
-]
+data = {
+    'next_id' : 3,
+    'POSTS' : [
+        {"id": 1, "title": "First post", "content": "This is the first post."},
+        {"id": 2, "title": "Second post", "content": "This is the second post."},
+    ]
+}
 
+def is_valid_post(post_title: str, post_content: str) -> bool:
+    """
+    Checks if the post title and the content is valid
+    :param post_title: String of the post title
+    :param post_content: String of the post content
+    :return: Boolean value of the check
+    """
+    correct_title_len = 50 >= len(post_title) > 0
+    correct_content_len = 500 > len(post_content) > 0
+    return correct_title_len and correct_content_len
+
+def get_next_id():
+    """
+    Returns the next id available
+    :return next_id: Integer value of the next id
+    """
+    return  data["next_id"]
+
+
+def create_post(post_title: str, post_content: str) -> None:
+    """
+    Creates a new post
+    :param post_title: String of the post title
+    :param post_content: String of the post content
+    """
+    data['POSTS'].append({
+        "id": get_next_id(),
+         "title": post_title,
+         "content": post_content
+    })
+    data['next_id'] += 1
+    return
 
 @app.route('/api/posts', methods=['GET'])
 def get_posts():
-    return jsonify(POSTS)
+    """
+    If GET-request: Returns a list of all posts
+    If POST request: Creates a new post
+    """
+    if request.method == 'POST':
+        title = request.json.get("title")
+        content = request.json.get("content")
+        if is_valid_post(title, content):
+            create_post(title, content)
+            return f"Post {title} created", 201
+
+    return jsonify(data['POSTS'])
 
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5002, debug=True)
+    app.run(host="0.0.0.0"
+                 "l;'./", port=5002, debug=True)
