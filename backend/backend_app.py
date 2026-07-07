26158bc68de5c8d0941431ba058883a9002181c6
Yasin Akkus <akkusoft@gmail.com>
2026-07-07 07:46:57 +0200

    'Delete' functionality created. Deleting the post if it exists with the given post id. serving a 404 Not found if the post with the given post id doesn't exist.

--- backend/backend_app.py
+++ backend/backend_app.py
@@ -5,12 +5,40 @@ app = Flask(__name__)
 CORS(app)  # This will enable CORS for all routes
 
 data = {
-    'next_id' : 3,
-    'POSTS' : [
-        {"id": 1, "title": "First post", "content": "This is the first post."},
-        {"id": 2, "title": "Second post", "content": "This is the second post."},
-    ]
+    'next_id' : 3
 }
+
+POSTS = [
+    {"id": 1, "title": "First post", "content": "This is the first post."},
+    {"id": 2, "title": "Second post", "content": "This is the second post."},
+]
+
+def fetch_all():
+    """
+    Returns a list of all posts
+    :return posts: List of all posts:
+    """
+    return POSTS
+
+def get_post_by_id(post_id: int) -> dict | None:
+    """
+    Returns a post, if it exists.
+    :param post_id:
+    :return post: Dictionary of the post or None:
+    """
+    for post in POSTS:
+        if post["id"] == post_id:
+            return post
+    return None
+
+def set_posts(posts_list: list[dict]):
+    """
+    Sets a list of posts
+    :param posts_list:
+    """
+    POSTS.clear()
+    for post in posts_list:
+        POSTS.append(post)
 
 def is_valid_post(post_title: str, post_content: str) -> bool:
     """
@@ -37,13 +65,24 @@ def create_post(post_title: str, post_content: str) -> None:
     :param post_title: String of the post title
     :param post_content: String of the post content
     """
-    data['POSTS'].append({
+    POSTS.append({
         "id": get_next_id(),
          "title": post_title,
          "content": post_content
     })
     data['next_id'] += 1
     return
+
+def remove_post(post_id: int) -> int | None:
+    """
+    Deletes a post
+    :param post_id: int id of the post
+    """
+    found_post = get_post_by_id(post_id)
+    if found_post:
+        POSTS.remove(found_post)
+        return post_id
+    return None
 
 @app.route('/api/posts', methods=['GET'])
 def get_posts():
@@ -58,8 +97,26 @@ def get_posts():
             create_post(title, content)
             return f"Post {title} created", 201
 
-    return jsonify(data['POSTS'])
+    return jsonify(POSTS)
 
+@app.route('/api/posts/<int:post_id>', methods=['PUT'])
+def update_post(post_id: int):
+    """
+    Updates a post
+    :param post_id:
+    """
+    updated_post = {}
+
+@app.route('/api/posts/<int:post_id>', methods=['DELETE'])
+def delete_post(post_id: int):
+    """
+    Deletes a post
+    :param post_id:
+    """
+    deleted_id = remove_post(post_id)
+    if deleted_id:
+        return f"Post with id <{post_id}> has been deleted successfully.", 200
+    return f"Post with id <{post_id}> does not exist.", 404
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0"
